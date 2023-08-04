0653e24fdbe41e74ba06182c1602807c95a634cf
aymane <aymane.atigui.0202@gmail.com>
2023-08-04 23:32:11 +0100

    configure DataBase

--- core/settings.py
+++ core/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'backend',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD':'root',
     }
 }
 
