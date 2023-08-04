c84378eee2e6c464d3e01f4224541513f739176e
aymane <aymane.atigui.0202@gmail.com>
2023-08-05 00:56:19 +0100

    Customize User Model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class MyUserManager(BaseUserManager):
+    def create_user(self, username ,email, first_name, last_name, password):
+        email = self.normalize_email(email)
+        user = self.model(
+            username = username,
+            email = email,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username ,email, first_name, last_name, password):
+        user = self.create_user(
+            username = username,
+            password = password,
+            email = email,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.save()
+
+        return user
+
+class User(AbstractUser):
+    
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(max_length=60, unique=True)
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ["email", "first_name", "last_name"]
+
+    objects = MyUserManager()
+
+    def __str__(self) -> str:
+        return f"{self.username} / {self.email}"
