9a70a8f0b2d4e2b45983b9a8aeb11570e961fcb0
aymane <aymane.atigui.0202@gmail.com>
2023-08-05 05:02:20 +0100

    Create Token using Simple JWT

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from account.views import AllUsers, SignUp
+from account.views import AllUsers, Signup
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('users/', AllUsers.as_view(), name="users"),
-    path('signup/', SignUp.as_view(), name="Sign_Up"),
+    path('signup/', Signup.as_view(), name="Sign_Up"),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,23 +1,31 @@
 from account.models import MyUser
 from account.serializer import UserSerializer
+from account.tokens import createToken
+from django.contrib.auth import authenticate
 
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.permissions import IsAuthenticated
+
 
 class AllUsers(GenericAPIView, ListModelMixin):
+    permission_classes = [IsAuthenticated]
     def get(self,request:Request):
         queryset = MyUser.objects.all()
         users = UserSerializer(queryset, many=True)
         return Response(users.data, status=status.HTTP_200_OK)
 
-class SignUp(GenericAPIView, CreateModelMixin):
+class Signup(GenericAPIView, CreateModelMixin):
     def post(self, request:Request):
         data = request.data
         serializer = UserSerializer(data=data)
         if serializer.is_valid():
-            serializer.save()
-            return Response({"Message" : "User has been created", "Data": serializer.data}, status= status.HTTP_201_CREATED)
+            user = serializer.save()
+            token = createToken(user)
+            return Response({"Message" : "User has been created", 'Token': token,"Data": serializer.data}, status= status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
